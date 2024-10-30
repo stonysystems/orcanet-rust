e16fabaa2fe9a92292cc6d89c2fb11cf12775f7e
Sethu98 <sethu.kumar7@gmail.com>
2024-10-30 19:40:49 -0400

    Proxy changes

--- file_transfer/src/http/http_proxy.rs
+++ file_transfer/src/http/http_proxy.rs
@@ -1,10 +1,11 @@
 use std::net::SocketAddr;
+use std::sync::Arc;
 
 use bytes::Bytes;
 use futures::channel::mpsc::Receiver;
 use futures::StreamExt;
 use http_body_util::{BodyExt, Full};
-use hyper::{Request, Response, StatusCode};
+use hyper::{Error, Request, Response, StatusCode};
 use hyper::body::{Body, Incoming};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
@@ -23,20 +24,134 @@ const ORCA_NET_CLIENT_ID_HEADER: &str = "orca-net-client-id";
 const ORCA_NET_AUTH_KEY_HEADER: &str = "orca-net-token";
 const PROXY_PORT: u16 = 3000;
 
-trait ProxyRequestHandler {
-    async fn serve(stream: TcpStream, io: TokioIo<TcpStream>);
-
-    async fn handle_request(request: Request<Incoming>) -> Result<Response<Full<Bytes>>, hyper::Error>;
-}
-
 struct ProxyProvider;
-impl ProxyRequestHandler for ProxyProvider {
-    async fn serve(stream: TcpStream, io: TokioIo<TcpStream>) {
-        todo!()
+
+// impl ProxyRequestHandler for ProxyProvider {
+//     async fn serve(&mut self, io: TokioIo<TcpStream>) {
+//         tokio::task::spawn(async move {
+//             if let Err(err) = http1::Builder::new()
+//                 .serve_connection(io, service_fn(|req| Self::handle_request(req)))
+//                 .await {
+//                 tracing::error!("Error serving connection: {:?}", err);
+//             }
+//         });
+//     }
+// }
+
+impl ProxyProvider {
+    pub async fn handle_request(
+        request: Request<Incoming>,
+    ) -> Result<Response<Full<Bytes>>, hyper::Error> {
+        // Get client id and auth token
+        tracing::info!("Request headers: {:?}", request.headers());
+
+        let client_id = match request.headers()
+            .get(ORCA_NET_CLIENT_ID_HEADER)
+            .and_then(|v| v.to_str().ok()) {
+            Some(client_id) => client_id,
+            None => {
+                return Ok(bad_request_with_err(
+                    OrcaNetError::InvalidClientId("Orca-Net-Client-ID not found in headers".to_string())
+                ));
+            }
+        };
+        let token_in_header = match request.headers()
+            .get(ORCA_NET_AUTH_KEY_HEADER)
+            .and_then(|v| v.to_str().ok()) {
+            Some(token) => token,
+            None => {
+                return Ok(bad_request_with_err(
+                    OrcaNetError::InvalidAuthToken("Orca-Net-Token not found in headers".to_string())
+                ));
+            }
+        };
+
+        // Validate token
+        let mut proxy_clients_table = ProxyClientsTable::new(None);
+        match proxy_clients_table.get_client_auth_token(client_id) {
+            Some(token) => {
+                if token_in_header != token {
+                    return Ok(bad_request_with_err(
+                        OrcaNetError::InvalidAuthToken("Orca-Net-Token mismatch".to_string())
+                    ));
+                }
+            }
+            None => {
+                return Ok(bad_request_with_err(
+                    OrcaNetError::InvalidClientId("Client is not registered".to_string())
+                ));
+            }
+        }
+
+        tracing::info!("Request body size {:?}", request.size_hint().exact());
+
+        // Send the request
+        let path = request.uri().path();
+        tracing::info!("Request path: {path}");
+
+        let client = Client::builder(hyper_util::rt::TokioExecutor::new())
+            .build(HttpsConnector::new());
+        let response = client.request(request)
+            .await
+            .unwrap();
+
+        let (parts, body) = response.into_parts();
+        let bytes = body.collect()
+            .await?
+            .to_bytes();
+
+        tracing::info!("Response body size: {:?}", bytes.len());
+
+        Ok(Response::from_parts(parts, Full::new(bytes)))
     }
 }
 
-struct ProxyUser {}
+struct ProxyClient {
+    config: ProxyClientConfig,
+}
+
+// impl ProxyRequestHandler for ProxyClient {
+//     async fn serve(&mut self, io: TokioIo<TcpStream>) {
+//         let config_clone = self.config.clone();
+//
+//         tokio::task::spawn(async move {
+//             if let Err(err) = http1::Builder::new()
+//                 .serve_connection(io, service_fn(|req| Self::handle_request(req, config_clone)))
+//                 .await {
+//                 tracing::error!("Error serving connection: {:?}", err);
+//             }
+//         });
+//     }
+// }
+
+impl ProxyClient {
+    pub async fn handle_request(
+        request: Request<Incoming>,
+        config: ProxyClientConfig)
+        -> Result<Response<Full<Bytes>>, Error> {
+        // Get token
+        tracing::info!("Request headers: {:?}", request.headers());
+
+        // Send the request through proxy
+        let path = request.uri().path();
+        tracing::info!("Request path: {path}");
+
+        let client = Client::builder(hyper_util::rt::TokioExecutor::new())
+            .build(HttpsConnector::new());
+        let response = client.request(request)
+            .await
+            .unwrap();
+
+        let (parts, body) = response.into_parts();
+        let bytes = body.collect()
+            .await?
+            .to_bytes();
+
+        tracing::info!("Response body size: {:?}", bytes.len());
+
+        Ok(Response::from_parts(parts, Full::new(bytes)))
+    }
+}
 
 fn bad_request_with_err(err: OrcaNetError) -> Response<Full<Bytes>> {
     let json_resp = json!({
@@ -48,97 +163,6 @@ fn bad_request_with_err(err: OrcaNetError) -> Response<Full<Bytes>> {
         .status(StatusCode::BAD_REQUEST)
         .body(Full::new(body))
         .expect("Couldn't build body")
-}
-
-async fn handle_provide_request(request: Request<Incoming>) -> Result<Response<Full<Bytes>>, hyper::Error> {
-    // Get client id and auth token
-    tracing::info!("Request headers: {:?}", request.headers());
-
-    let client_id = match request.headers()
-        .get(ORCA_NET_CLIENT_ID_HEADER)
-        .and_then(|v| v.to_str().ok()) {
-        Some(client_id) => client_id,
-        None => {
-            return Ok(bad_request_with_err(
-                OrcaNetError::InvalidClientId("Orca-Net-Client-ID not found in headers".to_string())
-            ));
-        }
-    };
-    let token_in_header = match request.headers()
-        .get(ORCA_NET_AUTH_KEY_HEADER)
-        .and_then(|v| v.to_str().ok()) {
-        Some(token) => token,
-        None => {
-            return Ok(bad_request_with_err(
-                OrcaNetError::InvalidAuthToken("Orca-Net-Token not found in headers".to_string())
-            ));
-        }
-    };
-
-    // Validate token
-    let mut proxy_clients_table = ProxyClientsTable::new(None);
-    match proxy_clients_table.get_client_auth_token(client_id) {
-        Some(token) => {
-            if token_in_header != token {
-                return Ok(bad_request_with_err(
-                    OrcaNetError::InvalidAuthToken("Orca-Net-Token mismatch".to_string())
-                ));
-            }
-        }
-        None => {
-            return Ok(bad_request_with_err(
-                OrcaNetError::InvalidClientId("Client is not registered".to_string())
-            ));
-        }
-    }
-
-    tracing::info!("Request body size {:?}", request.size_hint().exact());
-
-    // Send the request
-    let path = request.uri().path();
-    tracing::info!("Request path: {path}");
-
-    let client = Client::builder(hyper_util::rt::TokioExecutor::new())
-        .build(HttpsConnector::new());
-    let response = client.request(request)
-        .await
-        .unwrap();
-
-    let (parts, body) = response.into_parts();
-    let bytes = body.collect()
-        .await?
-        .to_bytes();
-
-    tracing::info!("Response body size: {:?}", bytes.len());
-
-    Ok(Response::from_parts(parts, Full::new(bytes)))
-}
-
-async fn handle_use_request(
-    request: Request<Incoming>,
-    proxy_client_config: ProxyClientConfig,
-) -> Result<Response<Full<Bytes>>, hyper::Error> {
-    // Get token
-    tracing::info!("Request headers: {:?}", request.headers());
-
-    // Send the request through proxy
-    let path = request.uri().path();
-    tracing::info!("Request path: {path}");
-
-    let client = Client::builder(hyper_util::rt::TokioExecutor::new())
-        .build(HttpsConnector::new());
-    let response = client.request(request)
-        .await
-        .unwrap();
-
-    let (parts, body) = response.into_parts();
-    let bytes = body.collect()
-        .await?
-        .to_bytes();
-
-    tracing::info!("Response body size: {:?}", bytes.len());
-
-    Ok(Response::from_parts(parts, Full::new(bytes)))
 }
 
 pub async fn start_http_proxy(mode: ProxyMode, mut receiver: Receiver<OrcaNetEvent>) {
@@ -166,14 +190,18 @@ pub async fn start_http_proxy(mode: ProxyMode, mut receiver: Receiver<OrcaNetEve
                 tracing::info!("Got listener accept");
                 let (stream, _) = stream_event.unwrap();
                 let io = TokioIo::new(stream);
+                let mode_clone = mode.clone();
 
                 tokio::task::spawn(async move {
                     if let Err(err) = http1::Builder::new()
-                        .serve_connection(io, service_fn(handle_provide_request))
+                        .serve_connection(io, service_fn(|req| {
+
+                    }))
                         .await {
                         tracing::error!("Error serving connection: {:?}", err);
                     }
                 });
+
             }
         }
     }
